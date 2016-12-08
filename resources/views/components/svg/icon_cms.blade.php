0cf3470da625e5faae5b2d89041633f43c7b5f10
Alexander Milde <alex@finekost.com>
2016-12-08 18:07:08 +0100

    icon für cms gebaut und animiert.

---
+++ resources/views/components/svg/icon_cms.blade.php
@@ -0,0 +1,17 @@
+<svg id="gfx-cms" width="100%"   viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g id="Icon-CMS">
+    <rect id="bg" fill="#DADADA" x="0" y="0" width="200" height="200"></rect>
+    <rect id="sidebar" fill="#3B3B3B" x="0" y="0" width="48" height="200"></rect>
+    <rect id="sections" fill="#DADADA" x="0" y="19" width="48" height="10"></rect>
+    <rect id="input_cpy" fill="#FFFFFF" x="58" y="76" width="130" height="107"></rect>
+    <rect id="input_hl" fill="#FFFFFF" x="58" y="39" width="130" height="24"></rect>
+    <rect id="image3" fill="#70C1B3" x="142" y="114" width="0" height="0"></rect>
+    <rect id="image2" fill="#FF1654" x="102" y="114" width="0" height="0"></rect>
+    <rect id="image1" fill="#247BA0" x="62" y="114" width="0" height="0"></rect>
+    <rect id="headline" fill="#3B3B3B" x="62" y="46" width="0" height="10"></rect>
+    <rect id="copy1" fill="#3B3B3B" x="62" y="83" width="0" height="10"></rect>
+    <rect id="copy2" fill="#3B3B3B" x="62" y="99" width="0" height="10"></rect>
+    <rect id="copy3" fill="#3B3B3B" x="62" y="149" width="0" height="10"></rect>
+    <rect id="copy4" fill="#3B3B3B" x="62" y="164" width="0" height="10"></rect>
+  </g>
+</svg>
