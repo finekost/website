e20a31691824bfe01f9c59f750593c11a1362afc
Alexander Milde <alex@finekost.com>
2016-12-09 17:08:24 +0100

    responsive icons

--- resources/views/components/expertise.blade.php
+++ resources/views/components/expertise.blade.php
@@ -4,8 +4,8 @@
   <p class="maintext">Unser Fachgebiet sind <strong>Webseiten</strong> und <strong>Apps</strong>, sowie web-basierte Softwarelösungen. Wir setzen auf zeitgemäße Technologien und Frameworks, rennen aber nicht jedem Trend hinterher.</p>
   <div class="container-expertise">
     @include('components.expertise-single', ['icon' => 'responsive', 'hl' => 'Response Websites', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
-    @include('components.expertise-single', ['icon' => 'responsive', 'hl' => 'Apps', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
+    @include('components.expertise-single', ['icon' => 'apps', 'hl' => 'Apps', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
     @include('components.expertise-single', ['icon' => 'cms', 'hl' => 'Content Management', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
-    @include('components.expertise-single', ['icon' => 'cms', 'hl' => 'Backends', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
+    @include('components.expertise-single', ['icon' => 'backend', 'hl' => 'Backends', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
   </div>
 </div>
