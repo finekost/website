fba511e88b73fc0cb2bb3efe424ab287dc900c07
Alex Milde <alex@finekost.com>
2017-07-19 18:14:37 +0200

    patched page title

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
+        <title>{{$case['title']}}</title>        
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css">
