8410859d770d892e253e3e43989f887355fe955b
Alex Milde <alex@finekost.com>
2017-07-26 13:15:23 +0200

    final layout fixes for desktop

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,26 @@
-<div class="bottom-content">
-    <div class="" style="float: left;">
-        <h2 class="headline">{{trans('home.headline')}}</h2>
-        <h3 class="subheadline">{{trans('home.copy')}}</h3>
-        <div class="buttons-wrap">
-            <a class="works-button" href="javascript:void(0)">{{trans('navi.cases')}}</a>
-            <a class="contact-button" href="{!! url('/team'); !!}">{{trans('navi.team')}}</a>
-        </div>
-    </div>
-    <img class="cat-img" src="img/sarah-dorweiler-128578.jpg" style="float: left;width: 57%; height; auto; margin-top: 100px;">
-    <div class="clearfix"></div>
-    <ul>
-      <li><a href="mailto:alex@finekost.com">{{trans('navi.mail')}}</a></li>
-      <li><a href="tel:+491789263669">{{trans('navi.phone')}}</a></li>
-      <li><a href="https://www.google.de/maps/place/finekost/@53.5502516,9.9995629,17z/data=!3m1!4b1!4m5!3m4!1s0x47b18ee242543b51:0x522f02110a961762!8m2!3d53.5502516!4d10.0017516">{{trans('navi.maps')}}</a></li>
-    </ul>
+@php
+$style = '';
+if(!empty($showInstant))
+  $style = 'opacity:1';
+@endphp
 
-    Ust-ID:268126828
+<div class="bottom-content" style="{{$style}}">
+  <h2>{{trans('home.headline')}}</h2>
+  <h3>{{trans('home.copy')}}</h3>
+
+  <ul class="main">
+      <li><a class="cases" href="#">{{trans('navi.cases')}}</a></li>
+      <li> | </li>
+      <li><a href="{!! url('/team'); !!}">{{trans('navi.team')}}</a></li>
+  </ul>
+
+  <ul class="sub" style="{{$style}}">
+    <li><a href="mailto:alex@finekost.com">{{trans('navi.mail')}}</a></li>
+    <li> – </li>
+    <li><a href="tel:+491789263669">{{trans('navi.phone')}}</a></li>
+    <li> – </li>
+    <li><a href="https://www.google.de/maps/place/finekost/@53.5502516,9.9995629,17z/data=!3m1!4b1!4m5!3m4!1s0x47b18ee242543b51:0x522f02110a961762!8m2!3d53.5502516!4d10.0017516">{{trans('navi.maps')}}</a></li>
+    <li class="sub">Ust-ID: 268126828</li>
+  </ul>
+
 </div>
