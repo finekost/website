4a5d3475c1a069bd2d2987f51d1b4c20e7825870
Alex Milde <alex@finekost.com>
2017-10-16 16:07:10 +0200

    title typo. o.O

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Webdvelopment & Appdevelopment made in Hamburg</title>
+    <title>Webdevelopment & Appdevelopment made in Hamburg</title>
     <script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/detectmobilebrowser.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/main.js') }}" type="text/javascript"></script>
