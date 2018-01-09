f190012f589dea42003110be8e1ddb7bb7943ff0
Alexander Milde <alex@finekost.com>
2018-01-09 22:27:19 +0100

    added masonry

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -1,7 +1,9 @@
 <div class="container projects">
   <h4>Projekte</h4>
+  <!--
   <h2>Eine kleine Auswahl unserer Arbeiten</h2>
   <p>Seit 2009 produzieren wir Webseiten, Apps und Backends. Wir haben ein paar im Detail beschrieben.</p>
   <div class="container-projects">projects images</div>
   <a class="btn">Mehr Arbeiten</a>
+-->
 </div>
