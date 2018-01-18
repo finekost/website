ad42a8df84479025ffc8a0949a5245e730fe6d3a
Alex Milde <alex@finekost.com>
2018-01-18 19:40:53 +0100

    typo fixes

--- resources/views/components/expertise.blade.php
+++ resources/views/components/expertise.blade.php
@@ -4,11 +4,11 @@
   <p class="maintext">Das Internet, Technik und gute digitale Kommunikation. In
     den Bereichen kennen wir uns aus. Zeitgemäße Technologien und Abläufe sind
     uns wichtig. Wir arbeiten oft für die Großen, gerne auch für den Mittelstand
-     und fallen im Idealfall garnicht auf. Unsere Kernkompetenzen:</p>
+     und fallen im Idealfall gar nicht auf. Unsere Kernkompetenzen:</p>
   <div class="container-expertise">
-    @include('components.expertise-single', ['icon' => 'responsive', 'hl' => 'Response Websites', 'cpy' => 'Egal welches Endgerät, die Website sollte gut aussehen. Das ist nicht immer einfach, aber "Einfach" ist ja auch langweilig - und daher eh nicht so unser Ding. '])
+    @include('components.expertise-single', ['icon' => 'responsive', 'hl' => 'Responsive Websites', 'cpy' => 'Egal welches Endgerät, die Website sollte gut aussehen. Das ist nicht immer einfach, aber "Einfach" ist ja auch langweilig - und daher eh nicht so unser Ding. '])
     @include('components.expertise-single', ['icon' => 'apps', 'hl' => 'Apps', 'cpy' => 'Egal ob <strong>Swift</strong> oder Objective-C wir haben einige Jahre Erfahrung bei der Nativen App-entwicklung. Android? Auch kein Problem! In unserem Netzwerk haben wir auch dafür Spezialisten.'])
     @include('components.expertise-single', ['icon' => 'cms', 'hl' => 'Content Management', 'cpy' => 'Wordpress, Typo3 oder Contao? Für viele große und auch kleine Kunden haben wir CMS Projekte umgesetzt. Gerne auch mit komplexen Plugins, die wir bei Bedarf selber schreiben.'])
-    @include('components.expertise-single', ['icon' => 'backend', 'hl' => 'Backends', 'cpy' => 'Videos Rendern, Pdfs generieren, Payments oder E-Cards? Länderspezifische 3D Daten weltweit für AR-Apps ausliefern? Alles schon gemacht. Meist mit Laravel (PHP) Immer mit viel Liebe zum Detail.'])
+    @include('components.expertise-single', ['icon' => 'backend', 'hl' => 'Backends', 'cpy' => 'Videos Rendern, Pdfs generieren, Payments oder E-Cards? Länderspezifische 3D Daten weltweit für AR-Apps ausliefern? Alles schon gemacht. Meist mit Laravel (PHP) immer mit viel Liebe zum Detail.'])
   </div>
 </div>
