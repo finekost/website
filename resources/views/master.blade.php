8acf27d1f05aec261eb0e722f0a121403413153b
Alex Milde <alex@finekost.com>
2018-01-18 18:08:14 +0100

    pagespeed optimizations

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,32 +2,48 @@
 <html lang="de">
 <head>
   <title>finekost: Webdevelopment &amp; Appdevelopment aus Hamburg</title>
-  <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-  <script async src="{{asset('js/webfont.js')}}"></script>
 
+  <script async src="{{asset('js/webfont.js')}}"></script>
   <script async src="{{asset('js/snap.svg-min.js')}}"></script>
   <script>
     window.onload = function () {
+
+      WebFont.load({
+        google: {
+          families: [ 'Bree+Serif',
+          'Raleway:300,400,700']
+        }
+      });
+
       svgAniCMS();
       svgAniResponsive();
       svgAniApps();
       svgAniBackend();
     }
   </script>
+
 </head>
 <body>
   <div class="container-outer">
     @yield('content')
   </div>
 
-  <script async>
-  WebFont.load({
-    google: {
-      families: [ 'Bree+Serif',
-      'Raleway:300,400,700']
-    }
-  });
-  </script>
+  <noscript id="deferred-styles">
+      <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"/>
+    </noscript>
+    <script>
+      var loadDeferredStyles = function() {
+        var addStylesNode = document.getElementById("deferred-styles");
+        var replacement = document.createElement("div");
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement)
+        addStylesNode.parentElement.removeChild(addStylesNode);
+      };
+      var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+      if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+      else window.addEventListener('load', loadDeferredStyles);
+    </script>
 </body>
 
 
