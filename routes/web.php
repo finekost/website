526d4c9f3ab11bbcdd51862c9a351969c6948505
Alexander Milde <alex@finekost.com>
2018-01-28 14:08:54 +0100

    fixed merge issue

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 Route::get('/', function () {
-<<<<<<< HEAD
   return view('home');
 });
 
 Route::get('/bueroplatz-mieten', function () {
     return view('office');
-=======
-    return view('home');
-    //return Response::view('home')->header('Cache-Control', 'max-age=2592000, public');
->>>>>>> develop
 });
