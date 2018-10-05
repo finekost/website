2af3a8c63754ca8973eb00812c0ccbdfd2e0953f
Alexander Milde <alex@finekost.com>
2018-10-05 15:16:33 +0200

    changed address

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,6 @@
 <div class="container footer">
   <ul class="left">
     <li class="hl">Finekost</li>
-    <li><a class="animated" href="tel:+494038650443">fon: 040 386 504 43</a></li>
     <li><a class="animated" href="tel:+491789263669">mobil: 0178 926 36 69</a></li>
   </ul>
   <!--
@@ -26,7 +25,7 @@
 -->
   <ul class="right">
     <li class="hl">Handcrafted with love in Hamburg, Germany.</li>
-    <li><a class="animated" href="https://www.google.de/maps/place/finekost/@53.5502516,9.9995629,17z/data=!3m1!4b1!4m5!3m4!1s0x47b18ee242543b51:0x522f02110a961762!8m2!3d53.5502516!4d10.0017516">Jakobikirchhof 9, 20095 Hamburg</a></li>
+    <li><a class="animated" href="https://www.google.de/maps/place/finekost/@53.552422,9.9994519,17z/data=!3m1!4b1!4m5!3m4!1s0x47b18ee242543b51:0x522f02110a961762!8m2!3d53.552422!4d10.001443">Lilienstraße 32, 20095 Hamburg</a></li>
     <li>UST-ID: 268126828</li>
   </ul>
 </div>
